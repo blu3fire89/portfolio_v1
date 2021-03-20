fb632ae4b80f23955bde309ce0780a41759edcc9
blu3fire <blu3fire89@gmail.com>
2021-03-20 22:32:27 +0800

    Porfolio left right

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import '../styles/Card.css';
 
-function Card({title, img, description, link}) {
+function Card({title, img, description, link, right}) {
     return (
         <div className="card">
-            <a href={link} target="_blank" rel="noreferrer"> 
-                <img className="cardImage" src={img} alt={title}/>
-                <div className="card-text">
-                    <h2>{title}</h2>
-                    <p>{description}</p>
-                </div>
-            </a>
-            
+            {right ? (
+                <a className="card-right" href={link} target="_blank" rel="noreferrer"> 
+                    <div className="card-text">
+                        <h2>{title}</h2>
+                        <p>{description}</p>
+                    </div>
+                    <img className="cardImage" src={img} alt={title}/>
+                </a>
+            ): (
+                
+                <a className="card-left" href={link} target="_blank" rel="noreferrer"> 
+                    <img className="cardImage" src={img} alt={title}/>
+                    <div className="card-text">
+                        <h2>{title}</h2>
+                        <p>{description}</p>
+                    </div>
+                    
+                </a>
+            )}
         </div>
     )
 }
--- src/components/PorfolioCard.js
+++ src/components/PorfolioCard.js
@@ -12,7 +12,8 @@ function PorfolioCard() {
             img="https://cdn-www.bluestacks.com/bs-images/Image_NF_LI_1.png.png"
             description="this is wowzers" />
 
-            <Card title="slack-clone" 
+            <Card right
+            title="slack-clone" 
             link="https://slack-clone-56f5a.web.app"
             img="https://i.pcmag.com/imagery/reviews/07td46ju7p6lLVb0QGwc5VF-6..1569479844.jpg"
             description="this is wowzers" />
@@ -22,7 +23,8 @@ function PorfolioCard() {
             img="https://neilpatel.com/wp-content/uploads/2017/04/instagram.jpg"
             description="this is wowzers" />
 
-            <Card title="spotify-clone" 
+            <Card right
+            title="spotify-clone" 
             link="https://spotify-clone-8751c.web.app"
             img="https://digital.hbs.edu/platform-digit/wp-content/uploads/sites/2/2020/04/spotify-logo-1920x1080-2.jpg"
             description="this is wowzers" />
